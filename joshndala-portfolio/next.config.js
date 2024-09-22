87c65a07d2e9abc1a9b8e7b52a383c1763b53deb
Josh <jndala246@gmail.com>
2024-09-22 14:35:10 -0400

    changes to setup github pages deployment

--- joshndala-portfolio/next.config.js
+++ joshndala-portfolio/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/personal-website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-website/' : '',
   images: {
     remotePatterns: [
       {
@@ -7,7 +10,9 @@ const nextConfig = {
         hostname: "**",
       },
     ],
+    unoptimized: true,
   },
+  output: 'export',
 };
 
 module.exports = nextConfig;

c6fed6003adfb5ba819f12265379e89c67ced723
Josh <jndala246@gmail.com>
2024-09-22 16:13:28 -0400

    updated config to get css styling

--- joshndala-portfolio/next.config.js
+++ joshndala-portfolio/next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/personal-website/joshndala-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-website/joshndala-portfolio/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/personal-website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-website/' : '',
   images: {
     unoptimized: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  transpilePackages: ['@material-tailwind/react'],
 };
 
 module.exports = nextConfig;

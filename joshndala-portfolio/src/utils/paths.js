10571ab934b0e309a1f124b569fdd3fc589ab44c
Josh <jndala246@gmail.com>
2025-02-16 15:27:33 -0500

    updated path utils

--- joshndala-portfolio/src/utils/paths.js
+++ joshndala-portfolio/src/utils/paths.js
@@ -1,9 +1,9 @@
 export function getStaticFilePath(filename) {
-    const basePath = process.env.NODE_ENV === 'production' ? '/personal-website' : '';
-    return `${basePath}/${filename}`;
-  }
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+  return `${basePath}/${filename}`;
+}
 
-  export function getSectionLink(sectionId) {
-    const basePath = process.env.NODE_ENV === 'production' ? '/personal-website' : '';
-    return `${basePath}/#${sectionId}`;
-  }
+export function getSectionLink(sectionId) {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+  return `${basePath}/#${sectionId}`;
+}
